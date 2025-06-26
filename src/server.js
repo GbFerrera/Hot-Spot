de494181f8028e0e9d80ab432df8d09632bfe9fd
GbFerrera <81236040biel@gmail.com>
2025-06-26 17:38:25 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -46,7 +46,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3132
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on Port ${PORT}`);
